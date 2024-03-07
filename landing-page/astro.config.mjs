dd9f105d0de88c065e3f2bc25a57de00f0f729ed
Miguel Matos <miguelamatos@ua.pt>
2024-03-07 14:12:17 +0000

    fix static.yml

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://mankings.github.io/",
+  base: "GIC-PROJECT",
   integrations: [tailwind(), icon()],
 });
