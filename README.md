64b55d9f9fa973648cb85efe765df6fb690d8da5
Miguel Matos <miguelamatos@ua.pt>
2024-04-14 22:13:29 +0100

    mariadb + folder struct

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 | 103341 | Miguel Matos    | miguelamatos@ua.pt    |
 | 103470 | Filipe Antão    | fantao@ua.pt          |
 | 98275  | Ricardo Antunes | ricardofantunes@ua.pt |
+
+## Utils
+
+```bash
+# base 64 encode
+echo -n "password123" | base64 -i -
+```
---
+++ scripts/apply.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+pushd mariadb
+kubectl apply -f pvc.yaml
+kubectl apply -f secrets.yaml
+kubectl apply -f statefulset.yaml
+kubectl apply -f service.yaml
+popd
+
+pushd drupal 
+kubectl apply -f secrets.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+popd
+
