095e95b6bfc87e82eb19f2b2608f382c86e4ae79
Miguel Matos <miguelamatos@ua.pt>
2024-04-18 23:55:07 +0100

    drupal + mariadb pods working, missing connection

--- config/drupal/entrypoint.sh
+++ config/drupal/entrypoint.sh
@@ -7,9 +7,4 @@ crond
 rm -rf /run/apache2/httpd.pid
 rm -rf /var/run/crond.pid
 
-# Copy the default settings.php file if it doesn't exist
-if [ ! -f /var/www/html/sites/default/settings.php ]; then
-  cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
-fi
-
 exec /usr/sbin/httpd -D FOREGROUND
