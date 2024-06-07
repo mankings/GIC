d434b825493b5f420451cce6c549b4d860770573
Miguel Matos <miguelamatos@ua.pt>
2024-06-07 18:16:32 +0100

    update

--- scripts/redis/apply-redis.sh
+++ scripts/redis/apply-redis.sh
@@ -1,6 +1,4 @@
 kubectl apply -f deploy/kubernetes/redis/secrets.yaml
 kubectl apply -f deploy/kubernetes/redis/service.yaml
 kubectl apply -f deploy/kubernetes/redis/config.yaml
-kubectl apply -f deploy/kubernetes/redis/deployment-master.yaml
-kubectl apply -f deploy/kubernetes/redis/deployment-sentinel.yaml
-kubectl apply -f deploy/kubernetes/redis/deployment-slave.yaml
+kubectl apply -f deploy/kubernetes/redis/deployment.yaml
