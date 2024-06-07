d86c829b1a612db1c4ed50b6cd73228278fb4691
Miguel Matos <miguelamatos@ua.pt>
2024-06-07 04:54:55 +0100

    ya

--- scripts/drupal/apply-drupal.sh
+++ scripts/drupal/apply-drupal.sh
@@ -3,4 +3,5 @@ kubectl apply -f deploy/kubernetes/drupal/service.yaml
 kubectl apply -f deploy/kubernetes/drupal/config.yaml
 kubectl apply -f deploy/kubernetes/drupal/pvc.yaml
 kubectl apply -f deploy/kubernetes/drupal/deployment.yaml
-kubectl apply -f deploy/kubernetes/drupal/autoscaling.yaml
+
+# kubectl apply -f deploy/kubernetes/drupal/autoscaling.yaml
